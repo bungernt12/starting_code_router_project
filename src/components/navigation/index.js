57eccc478e6339346d4263f2e91e8d79bb945ff3
Tague Zachary <taguezz@gmail.com>
2024-03-10 15:12:01 -0700

    Functional: routes to filter by type, route to detail page

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { getPetTypes } from '../../api/petfinder';
-import Logo from '../../assets/logo.svg';
-import Search from '../search';
+import React, { useEffect, useState } from "react";
+import { getPetTypes } from "../../api/petfinder";
+import Logo from "../../assets/logo.svg";
+import Search from "../search";
 
 // Import NavLink
 
@@ -24,11 +24,9 @@ const Navigation = () => {
         <Search />
       </div>
       <ul className="nav-links">
-        <li key={'all'}>
+        <li key={"all"}>
           {/* These links should be NavLink component and add a special active class name if its an active link */}
-          <a href="/"
-            className='nav-link'
-          >
+          <a href="/" className="nav-link">
             All Pets
           </a>
         </li>
@@ -36,14 +34,16 @@ const Navigation = () => {
           ? petTypes.map((type) => (
               <li key={type.name}>
                 {/* These links should be NavLink component and add a special active class name if its an active link */}
-                <a href={`/${type._links.self.href.split('/').pop()}`}
+                <a
+                  href={`/${type._links.self.href.split("/").pop()}`}
                   key={type.name}
-                  className='nav-link'               >
+                  className="nav-link"
+                >
                   {type.name}s
-                </a>{' '}
+                </a>{" "}
               </li>
             ))
-          : 'Loading...'}
+          : "Loading..."}
       </ul>
     </nav>
   );
